ea8c8e1b9561f6aa4f0b061d16d3f05c25872940
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-13 11:18:54 -0300

    Entendendo Fragments

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
       <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"/>
+      <List/>
     </div>
   );
 }
