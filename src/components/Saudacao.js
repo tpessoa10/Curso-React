a9c05bdfcb8dc91833659844ebb602c6176f9ee2
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-14 10:52:56 -0300

    Entendendo State Lift

---
+++ src/components/Saudacao.js
@@ -0,0 +1,12 @@
+function Saudacao({nome}){
+
+    function gerarSaudacao(algumNome){
+        return `Olá, ${algumNome}`
+    }
+
+    return (
+        <>{nome &&<p>{gerarSaudacao(nome)}</p>}</>
+    )
+}
+
+export default Saudacao
