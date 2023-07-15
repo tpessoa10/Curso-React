6ffe963ddf2bd5475a7d6d325d76469491384a7c
Thiago <tpessoa96@gmail.com>
2023-07-15 08:55:10 -0300

    Entendendo React Router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Home from './pages/Home';
 import Empresa from './pages/Empresa';
 import Contato from './pages/Contato';
-import Navbar from './components/Navbar';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='/empresa' element={<Empresa/>}/>
         <Route path='/contato' element={<Contato/>}/>
       </Routes>
+      <Footer/>
     </Router>
   );
 }
